941621725c3a6df7387c989b45f86ea6236058d6
Robert <robertbezerrads@gmail.com>
2022-02-13 20:07:27 -0300

    feature: tornando a lista dinamica

--- tarefas/lib/pages/todo_list_page.dart
+++ tarefas/lib/pages/todo_list_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class TodoListPage extends StatelessWidget {
-  const TodoListPage({Key? key}) : super(key: key);
+class TodoListPage extends StatefulWidget {
+  TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final TextEditingController todosController = TextEditingController();
+
+  List<String> todos = [];
 
   @override
   Widget build(BuildContext context) {
@@ -9,21 +18,73 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adicione uma tarefa',
-                    hintText: 'Ex: Estudar Flutter',
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: todosController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                        hintText: 'Ex: Estudar Flutter',
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {
+                      String text = todosController.text;
+                      setState(() {
+                        todos.add(text);
+                      });
+                      todosController.clear();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff00d7f3),
+                      padding: EdgeInsets.all(16),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Flexible(
+                child: SizedBox(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      for (String todo in todos)
+                        ListTile(
+                          title: Text(todo),
+                        ),
+                    ],
                   ),
                 ),
               ),
-              SizedBox(width: 8),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('+'),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Você possui 0 tarefas pendentes',
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff00d7f3),
+                      padding: EdgeInsets.all(16),
+                    ),
+                    child: Text('Limpar tudo'),
+                  ),
+                ],
               ),
             ],
           ),
